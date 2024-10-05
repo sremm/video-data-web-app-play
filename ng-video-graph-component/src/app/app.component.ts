4a768aef839d24e8a68b9db2b193a327baed7b9a
Sten Remmelg <sten.remmelg@gmail.com>
2024-10-05 20:53:23 +0300

    add common module for number pipe

--- ng-video-graph-component/src/app/app.component.ts
+++ ng-video-graph-component/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { VideoGraphComponent } from './video-graph/video-graph.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, VideoGraphComponent],
+  imports: [RouterOutlet, VideoGraphComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
