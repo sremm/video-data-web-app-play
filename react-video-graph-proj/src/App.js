2a64bb64deabb585a4020a2e6db2b33e3f9f3576
Sten Remmelg <sten.remmelg@gmail.com>
2024-10-06 14:58:19 +0300

    add a component

--- react-video-graph-proj/src/App.js
+++ react-video-graph-proj/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import VideoGraphComponent from './components/video-graph';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <div>Work in progress</div>
+        <VideoGraphComponent />
       </header>
     </div>
   );
---
+++ react-video-graph-proj/src/components/video-graph.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import './MyComponent.css';
+
+function VideoGraphComponent() {
+  return (
+    <div className="video-graph-component">
+      <h2>My New Component</h2>
+      <p>This is a new component.</p>
+    </div>
+  );
+}
+
+export default VideoGraphComponent;
